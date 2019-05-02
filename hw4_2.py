083b711a7100073a15983cec1661fead582f4991
tfchan <cseelemon@gmail.com>
2019-05-02 23:24:47 +0800

    Assign labels and calculate result statistic

--- hw4_2.py
+++ hw4_2.py
@@ -39,7 +39,7 @@ def log_(x):
     return np.log(np.where(x > 1e-50, x, 1e-50))
 
 
-def em_algorithm(x, n_cluster=2):
+def em_algorithm(x, y, n_cluster=2):
     """Perfrom EM on data x with n_cluster."""
     # Initialize parameters
     n_sample = x.shape[0]
@@ -70,7 +70,41 @@ def em_algorithm(x, n_cluster=2):
         cluster_proba = new_cluster_proba
         black_proba = new_black_proba
         iter += 1
+    print()
 
+    # Get cluster of each image
+    result = (np.dot(x, log_(black_proba.T))
+              + np.dot(1 - x, log_(1 - black_proba.T))
+              + log_(cluster_proba))
+    prediction = np.argmax(result, axis=1)
+    pred_y = np.zeros(n_sample)
+    for j in range(n_cluster):
+        c_j_idx = [idx for idx, pred in enumerate(prediction) if pred == j]
+        c_j_label = [y[k] for k in c_j_idx]
+        label, count = np.unique(c_j_label, return_counts=True)
+        pred_y[c_j_idx] = count.argmax()
+
+    # Output result
+    n_correct = 0
+    for j in range(n_cluster):
+        tp = ((pred_y == j) & (y == j)).sum()
+        tn = ((pred_y != j) & (y != j)).sum()
+        fp = ((pred_y == j) & (y != j)).sum()
+        fn = ((pred_y != j) & (y == j)).sum()
+        n_correct += tp
+        print(f'Confusion matrix {j}:')
+        print(f'\t\tPredict number {j}\tPredict not number {j}')
+        print(f'Is number {j}\t\t{tp}\t\t\t{fn}')
+        print(f"Isn't number {j}\t\t{fp}\t\t\t{tn}\n")
+        print(f'Sensitivity (Successfully predict cluster 1):',
+              tp / (tp + fn))
+        print(f'Specificity (Successfully predict cluster 2):',
+              tn / (tn + fp))
+        print('\n------------------------------------------------------------')
+    print(f'Total iteration to converge: {iter}')
+    print(f'Total error rate: {1 - (n_correct / n_sample)}')
+
+    # Plot cluster imaginations
     plt.figure()
     for c in range(n_cluster):
         plt.subplot(2, 5, c + 1)
@@ -102,12 +136,12 @@ def main():
 
     # Convert data
     tr_x = imgs2features(args.tr_image)
-    ts_x = imgs2features(args.ts_image)
+    # ts_x = imgs2features(args.ts_image)
     tr_y = args.tr_label
-    ts_y = args.ts_label
+    # ts_y = args.ts_label
 
     # Perform EM
-    em_algorithm(tr_x, n_cluster=10)
+    em_algorithm(tr_x, tr_y, n_cluster=10)
 
 
 if __name__ == '__main__':
